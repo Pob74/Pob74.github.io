dc7bd75ff65f7689b577d660fa09892c509631be
Pob74 <zeljkohaberstok@gmail.com>
2021-01-30 11:32:15 +0100

    first verion

---
+++ precache-manifest.b9f81e129b63cd80fee129b489a4b100.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7994e9ecbe39794f3102",
+    "url": "/css/app.5ec4b94c.css"
+  },
+  {
+    "revision": "38e7ad302b9b0183afeb",
+    "url": "/css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "3a09336c559cdbbb143dee274a7f7ac2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7994e9ecbe39794f3102",
+    "url": "/js/app.a1bc3cc2.js"
+  },
+  {
+    "revision": "38e7ad302b9b0183afeb",
+    "url": "/js/chunk-vendors.0327e4cb.js"
+  },
+  {
+    "revision": "9e2379bb204940efc20ad0d83a8cfbc9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
